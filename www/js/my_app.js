143c4572f14381604db7a313a1fc79f5b646cb29
Shaizan <shaizan.k@hotmail.com>
2020-02-12 17:17:17 -0700

    Fixed margin for self

--- www/js/my_app.js
+++ www/js/my_app.js
@@ -14,6 +14,14 @@ var app = new Framework7({
 });
 
 var mainView = app.views.create(".view-main");
+var $$ = Dom7;
+
+$$('.convert-form-to-data').on('click', function(){
+  var formData = app.form.convertToData('#my-form');
+  alert(JSON.stringify(formData));
+});
+
+
 
 /*
 $(".left").on("click",function(){
